80748bce5cb1dc50a7a3ab3c064aac6f1fafa1f5
lyy320617627 <ly320617627@163.com>
2021-11-01 15:40:42 +0800

    mybatis-参数传入

--- ch04-param/src/main/java/com/lyy/dao/StudentDao.java
+++ ch04-param/src/main/java/com/lyy/dao/StudentDao.java
@@ -1,8 +1,11 @@
 package com.lyy.dao;
 
 import com.lyy.domain.Student;
+import com.lyy.vo.QueryParam;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: mybatis-course
@@ -12,6 +15,30 @@ import java.util.List;
  **/
 
 public interface StudentDao {
-    List<Student> selectStudents();
-    int insertStudent(Student student);
+     /**
+      * 一个简单类型的参数：
+      * 简单类型：mybatis把java的基本数据类型和String都叫做简单数据类型
+      * 在mapper文件获取简单类型的一个参数的值，使用#{任意字符}
+      */
+     public Student selectStudentById(Integer id);
+     /**
+      * 多个参数：命名参数，在形参定义的1前面，加入@Param("自定义参数名称")
+      */
+     List<Student> selectMulitParam(@Param("myname") String name, @Param("myage")Integer age);
+     /**
+      * 多个参数，使用java对象作为接口中方法的参数
+      *
+      */
+     List<Student> selectMulitObject(QueryParam param);
+     /**
+      * 多个参数--简单类型：按位置传值
+      * mybatis3.4之前，使用#{0}，#{1}表示，版本3.4之后使用#{arg0}，#{arg1}
+      */
+     List<Student> selectMulitPosition(String name,Integer age);
+     /**
+      * 多个参数，使用mapper存放多个值
+      */
+     List<Student> selectMulitByMap(Map<String,Object> map);
+     List<Student> selectUse$order(@Param("colName") String colName);
+
 }
--- ch04-param/src/test/java/com/lyy/TestMybatis.java
+++ ch04-param/src/test/java/com/lyy/TestMybatis.java
@@ -3,10 +3,14 @@ package com.lyy;
 import com.lyy.dao.StudentDao;
 import com.lyy.domain.Student;
 import com.lyy.utils.MyBatisUtils;
+import com.lyy.vo.QueryParam;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import javax.swing.text.AbstractDocument;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: mybatis-course
@@ -17,19 +21,65 @@ import java.util.List;
 
 public class TestMybatis {
     @Test
-    public void testSelectStudents(){
-        /**
-         * 使用mybatis的动态代理机制，使用SqlSession.getMapper(dao接口)
-         * getMapper能够获取dao接口对应的实现类对象.
-         */
+    public void testSelectStudentById(){
+
         SqlSession sqlSession= MyBatisUtils.getSqlSession();
         StudentDao dao=sqlSession.getMapper(StudentDao.class);
-        //调用dao的方法，执行数据库的操作
-        List<Student> students=dao.selectStudents();
-        for (Student stu:students){
-            System.out.println("学生="+stu);
-        }
-
+        Student student = dao.selectStudentById(1001);
+        System.out.println(student);
     }
+     @Test
+    public void testSelectMulitParam(){
+        SqlSession sqlSession=MyBatisUtils.getSqlSession();
+        StudentDao dao=sqlSession.getMapper(StudentDao.class);
+         List<Student> studentList = dao.selectMulitParam("李四", 20);
+         for (Student stu:studentList){
+             System.out.println("学生="+stu);
+         }
+         sqlSession.close();
+     }
+     @Test
+    public void testSelectMulitObject(){
+        SqlSession sqlSession=MyBatisUtils.getSqlSession();
+        StudentDao dao=sqlSession.getMapper(StudentDao.class);
+         QueryParam queryParam=new QueryParam();
+         queryParam.setParamName("张三");
+         queryParam.setParamAge(20);
+         List<Student> studentList = dao.selectMulitObject(queryParam);
+         for (Student stu:studentList){
+             System.out.println("学生的信息="+stu);
+         }
+     }
+     @Test
+    public void testSelectMulitPosition(){
+        SqlSession sqlSession=MyBatisUtils.getSqlSession();
+        StudentDao dao=sqlSession.getMapper(StudentDao.class);
+         List<Student> studentList = dao.selectMulitPosition("李四", 20);
+         for (Student stu:studentList){
+             System.out.println("查询到的学生信息="+stu);
+         }
+     }
+     @Test
+    public void testSelectMulitByMap(){
+        SqlSession sqlSession=MyBatisUtils.getSqlSession();
+         StudentDao dao = sqlSession.getMapper(StudentDao.class);
+         Map<String,Object> map=new HashMap<>();
+         map.put("myname","张三");
+         map.put("myage",20);
+         List<Student> studentList = dao.selectMulitByMap(map);
+         for (Student student:studentList){
+             System.out.println("查询的学生信息="+student);
+         }
+     }
+     @Test
+    public void testSelectUser$order(){
+        SqlSession sqlSession=MyBatisUtils.getSqlSession();
+         StudentDao dao = sqlSession.getMapper(StudentDao.class);
+         List<Student> studentList = dao.selectUse$order("name");
+         for (Student student:studentList){
+             System.out.println("学生的信息="+student);
+         }
+         sqlSession.close();
 
+     }
 }
